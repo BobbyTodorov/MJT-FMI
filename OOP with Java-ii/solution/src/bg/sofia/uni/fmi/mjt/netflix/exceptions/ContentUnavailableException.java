0fe02b24a8e7fef533c73aa9b4f1e1ea4cb6b416
Борислав Тодоров <btodorov98@gmail.com>
2021-02-23 13:29:34 +0200

    Delete OOP with Java-ii directory

--- OOP with Java-ii/solution/src/bg/sofia/uni/fmi/mjt/netflix/exceptions/ContentUnavailableException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.netflix.exceptions;
-
-public class ContentUnavailableException extends Exception {
-    public ContentUnavailableException(String message){
-        super(message);
-    }
-}
