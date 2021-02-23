7984ec3d5290471bd79d020e89f03b8f604e13d4
Борислав Тодоров <btodorov98@gmail.com>
2021-02-23 13:28:05 +0200

    Delete Clean Code and Collections directory

--- Clean Code and Collections/solution/src/bg/sofia/uni/fmi/mjt/smartcity/hub/IllegalArgumentException.java
+++
@@ -1,7 +0,0 @@
-package bg.sofia.uni.fmi.mjt.smartcity.hub;
-
-public class IllegalArgumentException extends RuntimeException {
-    public IllegalArgumentException(String msg){
-        super(msg);
-    }
-}
